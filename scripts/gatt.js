b1ee2e01438c78db4721557f6bd251ba62466e65
Kevin Hill <khill@microsoft.com>
2017-05-11 22:03:07 -0700

    adding to gatt.js

--- scripts/gatt.js
+++ scripts/gatt.js
@@ -3,7 +3,6 @@ var bluetooth;
 var genericAttributeProfile;
 var enumeration;
 var background;
-var serviceProvider;
 var core;
 var foundation;
 var streams;
@@ -17,6 +16,7 @@ var operand1 = 0;
 var operator = 0;
 var resultVal = 0;
 var peripheralSupported = null;
+var serviceProvider;
 
 // Wait for the DOM to finish loading
 document.addEventListener("DOMContentLoaded", function () {
@@ -65,7 +65,7 @@ function createServiceCompleted(localService) {
     // TODO: implement 
     serviceProvider = localService.serviceProvider;
     var op1CharacteristicUuid = '20751f04-d067-4033-8352-24895943587d';
-    var op2CharacteristicUuid ='760d4884-717e-4316-80f1-75235adb1d7a'
+    var op2CharacteristicUuid = '760d4884-717e-4316-80f1-75235adb1d7a';
     var operatorCharacteristicUuid = 'f770790e-628b-410f-9b5c-39a6bd292b65';
     var resultCharacteristicUuid = '79586dd3-f6d7-48e0-945e-511d07dcca54';
 
@@ -77,38 +77,82 @@ function createServiceCompleted(localService) {
 
     // Operator
     var gattOperatorParameters = new genericAttributeProfile.GattLocalCharacteristicParameters();
-    gattOperatorParameters.userDescription = "Operator Charateristic";
+    gattOperatorParameters.userDescription = "Operator Characteristic";
     gattOperatorParameters.characteristicProperties = genericAttributeProfile.GattCharacteristicProperties.write | genericAttributeProfile.GattCharacteristicProperties.writeWithoutResponse;
     gattOperatorParameters.writeProtectionLevel = genericAttributeProfile.GattProtectionLevel.plain;
 
     // Result
     var gattResultParameters = new genericAttributeProfile.GattLocalCharacteristicParameters();
-    gattResultParameters.userDescription = "Result Charateristic";
+    gattResultParameters.userDescription = "Result Characteristic";
     gattResultParameters.characteristicProperties = genericAttributeProfile.GattCharacteristicProperties.read | genericAttributeProfile.GattCharacteristicProperties.notify;
     gattResultParameters.writeProtectionLevel = genericAttributeProfile.GattProtectionLevel.plain;
 
-    serviceProvider.service.createCharacteristicAsync(op1CharacteristicUuid, gattOperatorParameters)
+    // Create Operand Characteristic for operand 1
+    serviceProvider.service.createCharacteristicAsync(op1CharacteristicUuid, gattOperandParameters)
         .then(createCharateristicCompleted, createCharacteristicError)
-        .done();
-    //addServiceCharacteristics();
+        .done(function () {
+            console.log("Promise is finished for createCharacteristic Async")
+        });
+
+    // Create Operand Characteristic for operand 2
+    serviceProvider.service.createCharacteristicAsync(op2CharacteristicUuid, gattOperandParameters)
+        .then(createCharateristicCompleted, createCharacteristicError)
+        .done(function () {
+            console.log("Promise is finished for createCharacteristic Async")
+        });
+
+    // Create Operand Characteristic for operator
+    serviceProvider.service.createCharacteristicAsync(operatorCharacteristicUuid, gattOperatorParameters)
+        .then(createCharateristicCompleted, createCharacteristicError)
+        .done(function () {
+            console.log("Promise is finished for createCharacteristic Async")
+        });
+
+    // Create an integer format
+    var intFormat = genericAttributeProfile.GattPresentationFormat.fromParts(8, 0, 0, 0, 0);
+    gattResultParameters.presentationFormats.append(intFormat);
+
+    // Create Operand Characteristic for result
+    serviceProvider.service.createCharacteristicAsync(resultCharacteristicUuid, gattResultParameters)
+        .then(createCharateristicCompleted, createCharacteristicError)
+        .done(function () {
+            console.log("Promise is finished for createCharacteristic Async")
+        });
+
+    //GattServiceProviderAdvertisingParameters
+    //serviceProvider
+    var advParamters = new genericAttributeProfile.GattServiceProviderAdvertisingParameters();
+    advParamters.isConnectable = peripheralSupported;
+    advParamters.isDiscoverable = true;
+
+    serviceProvider.addEventListener("advertisementstatuschanged", serviceProviderAdvertismentStatusChanged);
+    serviceProvider.startAdvertising(advParamters);
+
 }
 
-function createCharateristicCompleted(result) {
+function createCharateristicCompleted(result, params) {
     switch (result.characteristic.userDescription) {
-        case "Operator Charateristic":
-            op1Characteristic = result.characteristic;
+        case "Operand Characteristic":
+            if (result.characteristic.uuid == '20751f04-d067-4033-8352-24895943587d') {
+                op1Characteristic = result.characteristic;
+                op1Characteristic.addEventListener("writerequested", op1WriteRequested);
+            } else if (result.characteristic.uuid == '760d4884-717e-4316-80f1-75235adb1d7a') {
+                op2Characteristic = result.characteristic;
+                op2Characteristic.addEventListener("writerequested", op2WriteRequested);
+            }
             break;
-        case "Operand Charateristic":
-
+        case "Operator Characteristic":
+            operatorCharacteristic = result.characteristic;
+            operatorCharacteristic.addEventListener("writerequested", operatorWriteRequested);
             break;
-        case "Result Charateristic":
-
+        case "Result Characteristic":
+            resultCharacteristic = result.characteristic;
+            resultCharacteristic.addEventListener("readrequested", resultReadRequested);
+            resultCharacteristic.addEventListener("subscribedclientschanged", resultSubscribedClientsChanged);
             break;
         default:
             console.log("ERROR: No Case Matched for createCharateristic!");
-
     }
-  
 }
 
 function createCharacteristicError(result) {
@@ -136,6 +180,29 @@ function bluetoothAdapterError(error) {
 
 }
 
+function op1WriteRequested(obj) {
+
+}
+
+function op2WriteRequested(obj) {
+
+}
+
+function operatorWriteRequested(obj) {
+
+}
+
+function resultReadRequested(obj) {
+
+}
+
+function resultSubscribedClientsChanged(obj) {
+
+}
+
+function serviceProviderAdvertismentStatusChanged(obj) {
+
+}
 //
 // enums
 //
