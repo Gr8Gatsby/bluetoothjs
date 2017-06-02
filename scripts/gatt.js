a70a52cdbbce24df092c549d247551e9c9c6f3e6
Kevin Hill <khill@microsoft.com>
2017-06-01 22:16:26 -0700

    updating gatt

--- scripts/gatt.js
+++ scripts/gatt.js
@@ -1,220 +1,205 @@
-// Store Windows namespace objects
-var bluetooth;
-var genericAttributeProfile;
-var enumeration;
-var background;
-var core;
-var foundation;
-var streams;
-
-// Global Variables
-var op1Characteristic = null;
-var op2Characteristic = null;
-var resultCharacteristic = null;
-var operand1 = 0;
-var operand1 = 0;
-var operator = 0;
-var resultVal = 0;
-var peripheralSupported = null;
-var serviceProvider;
-
-// Wait for the DOM to finish loading
-document.addEventListener("DOMContentLoaded", function () {
-    // Assign namespaces to shortcuts
-    bluetooth = Windows.Devices.Bluetooth;
-    genericAttributeProfile = Windows.Devices.Bluetooth.GenericAttributeProfile;
-    enumeration = Windows.Devices.Enumeration;
-    background = Windows.ApplicationModel.Background;
-    // from CS sample
-    core = Windows.ApplicationModel.Core;
-    foundation = Windows.Foundation;
-    streams = Windows.Storage.Streams;
-
-    // Throw error if object doesn't exist
-    if (typeof bluetooth == 'undefined') {throw "Bluetooth does not exist.";}
-    if (typeof genericAttributeProfile == 'undefined') {throw "GenericAttributeProfile does not exist.";}
-    if (typeof enumeration == 'undefined') {throw "Enumeration does not exist.";}
-    if (typeof background == 'undefined') {throw "Background does not exist.";}
-    if (typeof core == 'undefined') { throw "Core does not exist"; }
-    if (typeof foundation == 'undefined') { throw "Foundation does not exist"; }
-    if (typeof streams == 'undefined') { throw "Streams does not exist"; }
-
-    // Need to create a guid for the service
-    var uuid = '386c51fd-6806-447f-9bac-f4d491cf3731'; // generated from: https://guidgenerator.com
-
-    // Create a Gatt Service
-    genericAttributeProfile.GattServiceProvider.createAsync(uuid)
-        .then(createServiceCompleted, createServiceError)
-        .done(function () {
-            console.log("Promise is finished for creating GattServiceProvider.");
-        });
-
-    // Create a Bluetooth Adapter
-    bluetooth.BluetoothAdapter.getDefaultAsync()
-        .then(bluetoothAdapterCompleted, bluetoothAdapterError)
-        .done(function () {
-            console.log("Promise is finished for Bluetooth Adapter getDefaultAsync()");
-        });
-});
-
-
-//
-// GattServiceProvider createAsync Completed
-//
-function createServiceCompleted(localService) {
-    // TODO: implement 
-    serviceProvider = localService.serviceProvider;
-    var op1CharacteristicUuid = '20751f04-d067-4033-8352-24895943587d';
-    var op2CharacteristicUuid = '760d4884-717e-4316-80f1-75235adb1d7a';
-    var operatorCharacteristicUuid = 'f770790e-628b-410f-9b5c-39a6bd292b65';
-    var resultCharacteristicUuid = '79586dd3-f6d7-48e0-945e-511d07dcca54';
-
-    // Operand
-    var gattOperandParameters = new genericAttributeProfile.GattLocalCharacteristicParameters();
-    gattOperandParameters.userDescription = "Operand Characteristic";
-    gattOperandParameters.characteristicProperties = genericAttributeProfile.GattCharacteristicProperties.write | genericAttributeProfile.GattCharacteristicProperties.writeWithoutResponse;
-    gattOperandParameters.writeProtectionLevel = genericAttributeProfile.GattProtectionLevel.plain;
-
-    // Operator
-    var gattOperatorParameters = new genericAttributeProfile.GattLocalCharacteristicParameters();
-    gattOperatorParameters.userDescription = "Operator Characteristic";
-    gattOperatorParameters.characteristicProperties = genericAttributeProfile.GattCharacteristicProperties.write | genericAttributeProfile.GattCharacteristicProperties.writeWithoutResponse;
-    gattOperatorParameters.writeProtectionLevel = genericAttributeProfile.GattProtectionLevel.plain;
-
-    // Result
-    var gattResultParameters = new genericAttributeProfile.GattLocalCharacteristicParameters();
-    gattResultParameters.userDescription = "Result Characteristic";
-    gattResultParameters.characteristicProperties = genericAttributeProfile.GattCharacteristicProperties.read | genericAttributeProfile.GattCharacteristicProperties.notify;
-    gattResultParameters.writeProtectionLevel = genericAttributeProfile.GattProtectionLevel.plain;
-
-    // Create Operand Characteristic for operand 1
-    serviceProvider.service.createCharacteristicAsync(op1CharacteristicUuid, gattOperandParameters)
-        .then(createCharateristicCompleted, createCharacteristicError)
-        .done(function () {
-            console.log("Promise is finished for createCharacteristic Async")
-        });
-
-    // Create Operand Characteristic for operand 2
-    serviceProvider.service.createCharacteristicAsync(op2CharacteristicUuid, gattOperandParameters)
-        .then(createCharateristicCompleted, createCharacteristicError)
-        .done(function () {
-            console.log("Promise is finished for createCharacteristic Async")
-        });
-
-    // Create Operand Characteristic for operator
-    serviceProvider.service.createCharacteristicAsync(operatorCharacteristicUuid, gattOperatorParameters)
-        .then(createCharateristicCompleted, createCharacteristicError)
-        .done(function () {
-            console.log("Promise is finished for createCharacteristic Async")
-        });
-
-    // Create an integer format
-    var intFormat = genericAttributeProfile.GattPresentationFormat.fromParts(8, 0, 0, 0, 0);
-    gattResultParameters.presentationFormats.append(intFormat);
-
-    // Create Operand Characteristic for result
-    serviceProvider.service.createCharacteristicAsync(resultCharacteristicUuid, gattResultParameters)
-        .then(createCharateristicCompleted, createCharacteristicError)
-        .done(function () {
-            console.log("Promise is finished for createCharacteristic Async")
-        });
-
-    //GattServiceProviderAdvertisingParameters
-    //serviceProvider
-    var advParamters = new genericAttributeProfile.GattServiceProviderAdvertisingParameters();
-    advParamters.isConnectable = peripheralSupported;
-    advParamters.isDiscoverable = true;
-
-    serviceProvider.addEventListener("advertisementstatuschanged", serviceProviderAdvertismentStatusChanged);
-    serviceProvider.startAdvertising(advParamters);
-
+var api = {
+    bluetooth: null,
+    genericAttributionProfile: null,
+    enumeration: null,
+    deviceWatcher: null,
+    devices: new Array(),
+    services: new Array()
 }
 
-function createCharateristicCompleted(result, params) {
-    switch (result.characteristic.userDescription) {
-        case "Operand Characteristic":
-            if (result.characteristic.uuid == '20751f04-d067-4033-8352-24895943587d') {
-                op1Characteristic = result.characteristic;
-                op1Characteristic.addEventListener("writerequested", op1WriteRequested);
-            } else if (result.characteristic.uuid == '760d4884-717e-4316-80f1-75235adb1d7a') {
-                op2Characteristic = result.characteristic;
-                op2Characteristic.addEventListener("writerequested", op2WriteRequested);
-            }
-            break;
-        case "Operator Characteristic":
-            operatorCharacteristic = result.characteristic;
-            operatorCharacteristic.addEventListener("writerequested", operatorWriteRequested);
-            break;
-        case "Result Characteristic":
-            resultCharacteristic = result.characteristic;
-            resultCharacteristic.addEventListener("readrequested", resultReadRequested);
-            resultCharacteristic.addEventListener("subscribedclientschanged", resultSubscribedClientsChanged);
-            break;
-        default:
-            console.log("ERROR: No Case Matched for createCharateristic!");
+document.addEventListener("DOMContentLoaded", function () {
+    api.bluetooth = Windows.Devices.Bluetooth;
+    api.genericAttributionProfile = Windows.Devices.Bluetooth.GenericAttributeProfile;
+    api.enumeration = Windows.Devices.Enumeration;
+    api.deviceWatcher = Windows.Devices.Enumeration.DeviceWatcher;
+
+    if (typeof api.bluetooth === 'undefined') { throw "app.bluetooth is undefined"; }
+    if (typeof api.genericAttributionProfile === 'undefined') { throw "app.genericAttributionProfile is undefined"; }
+    if (typeof api.enumeration === 'undefined') { throw "app.enumeration is undefined"; }
+    if (typeof api.deviceWatcher === 'undefined') { throw "app.deviceWatcher is undefined"; }
+
+    api.devices = new Array();
+    api.bluetooth.BluetoothAdapter.getDefaultAsync()
+        .then(bluetoothAdapterCreated, bluetoothAdapterFailed);
+});
+
+function bluetoothAdapterCreated(adapter) {
+    renderDataToDOM("Bluetooth Address", adapter.bluetoothAddress);
+    renderDataToDOM("DeviceID", adapter.deviceId);
+    renderDataToDOM("Is Advertisement Offload Supported", adapter.isAdvertisementOffloadSupported);
+    renderDataToDOM("Is Peripheral Role Supported", adapter.isPeripheralRoleSupported);
+    renderDataToDOM("Is Low Energy Supported", adapter.isLowEnergySupported);
+    renderDataToDOM("Is Central Role Supported", adapter.isCentralRoleSupported);
+    renderDataToDOM("Is Classis Supported", adapter.isClassicSupported);
+
+    // start a device watcher
+    startDeviceWatcher();
+}
+
+function bluetoothAdapterFailed(error) {
+    renderDataToDOM("Error", error);
+}
+
+function startDeviceWatcher() {
+    var requestedProperties = ["System.Devices.Aep.DeviceAddress", "System.Devices.Aep.IsConnected","System.Devices.Aep.SignalStrength"];
+    api.deviceWatcher = Windows.Devices.Enumeration.DeviceInformation.createWatcher(
+        'System.Devices.Aep.ProtocolId:="{bb7bb05e-5972-42b5-94fc-76eaa7084d49}"',
+        requestedProperties,
+        Windows.Devices.Enumeration.DeviceInformationKind.associationEndpoint);
+
+    api.deviceWatcher.addEventListener("added", deviceWatcherAdded);
+    api.deviceWatcher.addEventListener("updated", deviceWatcherUpdated);
+    api.deviceWatcher.addEventListener("removed", deviceWatcherRemoved);
+    api.deviceWatcher.addEventListener("enumerationcompleted", deviceWatcherEnumerationCompleted);
+    api.deviceWatcher.addEventListener("stopped", deviceWatcherStopped);
+
+    api.deviceWatcher.start();
+}
+
+function deviceWatcherAdded(evt) {
+    //console.log("deviceWatcherAdded");
+    // Add device information to devices array
+    api.devices.push(evt.detail[0]);
+    if (evt.detail[0].name === "Tesla") {
+        renderDataToDOM("----", "----");
+        renderDataToDOM("name", evt.detail[0].name);
+        renderDeviceConnectButtonToDOM(evt.detail[0].id);
+        renderDataToDOM("id", evt.detail[0].id);
+        renderDataToDOM("Signal Strength", evt.detail[0].properties['System.Devices.Aep.SignalStrength']);
+        renderDataToDOM("Can Pair", evt.detail[0].pairing.canPair);
+        //renderDataToDOM("kind", evt.detail[0].kind);
+        //renderDataToDOM("Protection Level", evt.detail[0].pairing.protectionLevel);
+        //renderDataToDOM("Is Default", evt.detail[0].isDefault);
+        //renderDataToDOM("Is Enabled", evt.detail[0].isEnabled);
+
+        evt.detail[0].getGlyphThumbnailAsync()
+            .then(glyphCompleted, glyphError)
     }
 }
 
-function createCharacteristicError(result) {
+function glyphCompleted(image) {
+    if (image.size > 0) {
+        //TODO get image
+    }
+}
+
+function glyphError(obj) {
 
 }
 
-//
-// GattServiceProvider createAsync Error
-//
-function createServiceError(error) {
-    throw "Error creating GattServiceProvider.";
+function deviceWatcherUpdated(evt) {
+    if (evt.detail[0].name === "Tesla") {
+        renderDataToDOM("updated", evt.detail[0].id);
+    }
+    //console.log("deviceWatcherUpdated");
 }
 
-//
-// bluetoothAdapter createAsync Completed
-//
-function bluetoothAdapterCompleted(localAdapter) {
+function deviceWatcherRemoved(evt) {
+    if (evt.detail[0].name === "Tesla") {
+        renderDataToDOM("removed", evt.detail[0].id);
+    }
+    //console.log("deviceWatcherRemoved");
+}
+
+function deviceWatcherEnumerationCompleted(evt) {
+    console.log("deviceWatcherEnumerationCompleted");
+    console.log(api.devices);
+}
+
+function deviceWatcherStopped(evt) {
+    //console.log("deviceWatcherStopped");
+}
+
+// Helper function to display data
+function renderDataToDOM(label, text) {
+    var infoElem = document.createElement("div");
+    infoElem.setAttribute("class", "labelContainer");
+    var labelElem = document.createElement("span");
+    var textElem = document.createElement("span");
+
+    labelElem.setAttribute("class", "label");
+
+    var labelElemText = document.createTextNode(label + ":");
+    var textElemText = document.createTextNode(text);
+    labelElem.appendChild(labelElemText);
+    textElem.appendChild(textElemText);
+    infoElem.appendChild(labelElem);
+    infoElem.appendChild(textElem);
+    document.body.appendChild(infoElem);
+}
+
+function renderDeviceConnectButtonToDOM(id) {
+
+    var buttonElem = document.createElement("button");
+    buttonElem.setAttribute("id", id);
+    buttonElem.appendChild(document.createTextNode("Connect"));
+    buttonElem.addEventListener('click', function () { connectToDevice(id); });
+    document.body.appendChild(buttonElem);
+}
+
+function connectToDevice(id) {
+    var bluetoothLEDevice = api.bluetooth.BluetoothLEDevice.fromIdAsync(id)
+        .then(deviceFromIdAsyncCompleted, deviceFromIdAsyncError)
+        .done(console.log("Promise is finished for fromIdAsync"));
+}
+
+function deviceFromIdAsyncCompleted(device) {
+    device.getGattServicesAsync(api.bluetooth.BluetoothCacheMode.uncached)
+        .then(getGattServicesCompleted, getGattServicesError)
+        .done(console.log("Promise is finised for getGattServicesAsync"));
+}
+
+function deviceFromIdAsyncError(error) {
 
 }
 
-//
-// bluetoothAdapter createAsync Error
-//
-function bluetoothAdapterError(error) {
+function getGattServicesCompleted(service) {
+    if (service.status == 0) {
+        
+        if (service.services.length > 0) {
+            for (i = 0; i < service.services.length; i++) {
+                api.services.push(service.services[i]);
+                console.log("Opening " + service.services[i]);
+                service.services[i].getCharacteristicsAsync(api.bluetooth.GenericAttributeProfile.GattSharingMode.exclusive)
+                    .then(function (characteristic) {
+                        for (i = 0; i < characteristic.characteristics.length; i++) {
+                            characteristic.characteristics[i].readValueAsync(api.bluetooth.BluetoothCacheMode.uncached)
+                                .then(function (value) {
+                                    if (value.protocolError == 2) {
+                                        console.log("Error: ReadNotPermitted");
+                                    } else {
+                                        console.log("Value: " + value.value);
+                                    }
+                                })
+                        }
+                    }, function (error) {
+
+                    }) 
+                
+                    //.openAsync(api.bluetooth.GenericAttributeProfile.GattSharingMode.sharedReadOnly)
+                    //.then(function (gattServiceOpenAsyncCompleted, gattServiceOpenAsyncError) {
+                    //    if (gattServiceOpenAsyncCompleted == 1) {
+
+                    //    }
+                    //})
+                    //.done(console.log("Promise for gattSerivceOpenAsync is finished"))
+            }
+        } else {
+            console.log("GattService: No services available");
+        }
+    } else {
+        //TODO:?
+    }
+}
+
+function getGattServicesError(obj) {
 
 }
 
-function op1WriteRequested(obj) {
+function gattServiceOpenAsyncCompleted(connection) {
 
 }
 
-function op2WriteRequested(obj) {
+function gattServiceOpenAsyncError(obj) {
 
-}
-
-function operatorWriteRequested(obj) {
-
-}
-
-function resultReadRequested(obj) {
-
-}
-
-function resultSubscribedClientsChanged(obj) {
-
-}
-
-function serviceProviderAdvertismentStatusChanged(obj) {
-
-}
-//
-// enums
-//
-var calculatorCharacteristics = {
-    "Operand1":1,
-    "Operand2":2,
-    "Operator":3
-}
-
-var calculatorOperators = {
-    "Add":1,
-    "Subtract":2,
-    "Multiply":3,
-    "Divide":4
 }
